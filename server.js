d8ddd28273878392e34b083773a9cfc0e9eb946d
Tran Cong Thinh <cotitranzzy27@gmail.com>
2026-04-15 17:00:53 +0700

    fix port railway final

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.use("/api", require("./routes/content"));
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log("Server running on port " + PORT);
+  console.log("🚀 Server running on port " + PORT);
 });
