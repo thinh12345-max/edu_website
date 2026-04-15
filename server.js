2f3dff3db5df7d53913fd317ca569a276b4a9493
Tran Cong Thinh <cotitranzzy27@gmail.com>
2026-04-15 16:40:16 +0700

    fix PORT error

--- server.js
+++ server.js
@@ -19,4 +19,5 @@ app.use("/api", require("./routes/content"));
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port " + PORT);
+  console.log("MONGO_URI:", process.env.MONGO_URI);
 });
