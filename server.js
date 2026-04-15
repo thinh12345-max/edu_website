565f1a309cb607db5793da905a41aa3dcfee5d48
Tran Cong Thinh <cotitranzzy27@gmail.com>
2026-04-15 16:28:39 +0700

    fix PORT error

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.use("/api", require("./routes/content"));
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port " + PORT);
 });
